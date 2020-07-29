474a3929b5558fc9db872332aad405982df41969
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-07-28 21:45:52 -0700

    set up mySQL database with schema and seed data

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Colossal');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');

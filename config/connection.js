dceacde3ecffa328bf624203a15544b795d18f6f
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-07-30 22:08:50 -0700

    add routes to controllers and fix connection to database

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const dotenv = require("dotenv");
+const mysql = require("mysql")
 
 //set password to mySQL DB
 dotenv.config();
 const sqlPass = process.env.secret
 
 //Connection information for the sql database
-const connection ={
+const connection =mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -17,7 +18,15 @@ const connection ={
   // Your password
   password: sqlPass,
   database: "burgers_db"
-};
+});
 
+connection.connect(function(err){
+  if(err) {
+    console.error("Error connecting: "+ err.stack) 
+    return;
+  }
+  console.log("connected as id " + connection.threadId)
+
+})
 
 module.exports = connection;

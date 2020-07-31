7a64841f414d73074f5b72f312652a1f9139224d
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-07-30 21:13:59 -0700

    add model methods to burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,27 @@
 const orm = require("../config/orm");
+const connection = require("../config/connection");
 
 
 const burger= {
     //similar to a burger class with methods?
+
+    all:function(cb){
+        orm.selectAll("burgers", function(res){
+            cb(res)
+        })
+    },
+
+    create: function(col, vals, cb){
+        orm.insertOne("burgers", col, vals, function(res){
+            cb(res)
+        } )
+    },
+
+    update: function(objColVal, condition, cb){
+        orm.updateOne("burgers", objColVal, condition, function(res){
+            cb(res)
+        })
+    } 
 }
 
 

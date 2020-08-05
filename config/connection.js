f64382bef993b544afe7971bf430bb17ed9a98b3
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-08-04 22:47:37 -0700

    add environment variable for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -6,19 +6,23 @@ dotenv.config();
 const sqlPass = process.env.secret
 
 //Connection information for the sql database
-const connection =mysql.createConnection({
-  host: "localhost",
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+    const connection =mysql.createConnection({
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: sqlPass,
-  database: "burgers_db"
-});
+    // Your password
+    password: sqlPass,
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err){
   if(err) {
